9ccac89530a56274d7fcfce8c8736891168cc353
jay <jlaura@asu.edu>
2016-12-21 13:08:42 -0700

    Style updates

--- tests/mdisset.cpp
+++ tests/mdisset.cpp
@@ -6,8 +6,8 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-  if(argc<2){
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
     cerr << "Usage: " << argv[0] << " <isd file>" << endl;
     return 1;
   }
@@ -17,19 +17,18 @@ int main(int argc, char *argv[]){
   //Read the ISD file
   string line;
   string filename(argv[1]);
-  ifstream f (argv[1]);
+  ifstream f(argv[1]);
 
-      if (!f.is_open()) {
-          perror(("error while opening file " + filename).c_str());
-      }
-      while (getline(f, line)) {
-          cout << line << endl;
-          }
-      if (f.bad()) {
-          perror(("error while reading file " + filename).c_str());
-      }
-      f.close();
-
+  if (!f.is_open()) {
+    perror(("error while opening file " + filename).c_str());
+  }
+  while (getline(f, line)) {
+    cout << line << endl;
+  }
+  if (f.bad()) {
+    perror(("error while reading file " + filename).c_str());
+  }
+  f.close();
 
   return 0;
 }
