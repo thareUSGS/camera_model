6497c49cbbec9821ae395f708dbf4d15ab164f47
Ian <ihumphrey-usgs>
2017-01-06 13:38:49 -0700

    Added preliminary MdisNacSensorModelTest test. Implemented pure virtuals inherited from Model and GeometricModel in MdisNacSensorModel. Temporarily modified MdisNacSensorModel::getModelState for testing.

--- src/objs/MdisNacSensorModel.cpp
+++ src/objs/MdisNacSensorModel.cpp
@@ -1,7 +1,7 @@
 
 #include "MdisNacSensorModel.h"
 
-#include "csm/Error.h"
+#include <csm/Error.h>
 
 const std::string MdisNacSensorModel::_SENSOR_MODEL_NAME 
                                       = "ISIS_MDISNAC_USGSAstro_1_Linux64_csm30.so";
@@ -49,7 +49,6 @@ csm::EcefCoordCovar MdisNacSensorModel::imageToGround(const csm::ImageCoordCovar
                                   double heightVariance, double desiredPrecision, 
                                   double *achievedPrecision, 
                                   csm::WarningList *warnings) const {
-
     throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
       "Unsupported function",
       "MdisNacSensorModel::imageToGround");
@@ -188,3 +187,247 @@ std::vector<double> MdisNacSensorModel::getUnmodeledCrossCovariance(const csm::I
       "Unsupported function",
       "MdisNacSensorModel::getUnmodeledCrossCovariance");
 }
+
+
+
+
+csm::Version MdisNacSensorModel::getVersion() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getVersion");
+}
+
+
+std::string MdisNacSensorModel::getModelName() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getModelName");
+}
+
+
+std::string MdisNacSensorModel::getPedigree() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getPedigree");
+}
+
+
+std::string MdisNacSensorModel::getImageIdentifier() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getImageIdentifier");
+}
+
+
+void MdisNacSensorModel::setImageIdentifier(const std::string& imageId,
+                                            csm::WarningList* warnings) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setImageIdentifier");
+}
+
+
+std::string MdisNacSensorModel::getSensorIdentifier() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getSensorIdentifier");
+}
+
+
+std::string MdisNacSensorModel::getPlatformIdentifier() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getPlatformIdentifier");
+}
+
+
+std::string MdisNacSensorModel::getCollectionIdentifier() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getCollectionIdentifier");
+}
+
+
+std::string MdisNacSensorModel::getTrajectoryIdentifier() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getTrajectoryIdentifier");
+}
+
+
+std::string MdisNacSensorModel::getSensorType() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getSensorType");
+}
+
+
+std::string MdisNacSensorModel::getSensorMode() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getSensorMode");
+}
+
+
+std::string MdisNacSensorModel::getReferenceDateAndTime() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getReferenceDateAndTime");
+}
+
+
+std::string MdisNacSensorModel::getModelState() const {
+  /* commented out for testing the gtest framework
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getModelState");
+  */
+  return "";
+}
+
+
+void MdisNacSensorModel::replaceModelState(const std::string& argState) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::replaceModelState");
+}
+
+
+
+
+csm::EcefCoord MdisNacSensorModel::getReferencePoint() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getReferencePoint");
+}
+
+
+void MdisNacSensorModel::setReferencePoint(const csm::EcefCoord &groundPt) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setReferencePoint");
+}
+
+
+int MdisNacSensorModel::getNumParameters() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getNumParameters");
+}
+
+
+std::string MdisNacSensorModel::getParameterName(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterName");
+}
+
+
+std::string MdisNacSensorModel::getParameterUnits(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterUnits");
+}
+
+
+bool MdisNacSensorModel::hasShareableParameters() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::hasShareableParameters");
+}
+
+
+bool MdisNacSensorModel::isParameterShareable(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::isParameterShareable");
+}
+
+
+csm::SharingCriteria MdisNacSensorModel::getParameterSharingCriteria(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterSharingCriteria");
+}
+
+
+double MdisNacSensorModel::getParameterValue(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterValue");
+}
+
+
+void MdisNacSensorModel::setParameterValue(int index, double value) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setParameterValue");
+}
+
+
+csm::param::Type MdisNacSensorModel::getParameterType(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterType");
+}
+
+
+void MdisNacSensorModel::setParameterType(int index, csm::param::Type pType) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setParameterType");
+}
+
+
+double MdisNacSensorModel::getParameterCovariance(int index1, int index2) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getParameterCovariance");
+}
+
+
+void MdisNacSensorModel::setParameterCovariance(int index1, int index2, double covariance) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setParameterCovariance");
+}
+
+
+int MdisNacSensorModel::getNumGeometricCorrectionSwitches() const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getNumGeometricCorrectionSwitches");
+}
+
+
+std::string MdisNacSensorModel::getGeometricCorrectionName(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getGeometricCorrectionName");
+}
+
+
+void MdisNacSensorModel::setGeometricCorrectionSwitch(int index,
+                                                      bool value,
+                                                      csm::param::Type pType) {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::setGeometricCorrectionSwitch");
+}
+
+
+bool MdisNacSensorModel::getGeometricCorrectionSwitch(int index) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getGeometricCorrectionSwitch");
+}
+
+
+std::vector<double> MdisNacSensorModel::getCrossCovarianceMatrix(
+    const GeometricModel &comparisonModel,
+    csm::param::Set pSet,
+    const GeometricModelList &otherModels) const {
+  throw csm::Error(csm::Error::UNSUPPORTED_FUNCTION,
+                   "Unsupported function",
+                   "MdisNacSensorModel::getCrossCovarianceMatrix");
+    }
