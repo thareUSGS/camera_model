7aadda0c07f5cc6dd85227c0b54679403a91904b
MarjorieH <mah479@nau.edu>
2017-01-13 14:30:54 -0700

    Added SET functionality to main() in set.cpp, removed header file.

--- src/apps/set/set.cpp
+++ src/apps/set/set.cpp
@@ -1,15 +1,41 @@
-#include "set.h"
+#include "MdisPlugin.h"
+#include "MdisNacSensorModel.h"
+
+#include <Isd.h>
+#include <EcefCoord.h>
+#include <ImageCoord.h>
+
+#include <iostream>
 
 using namespace std;
 
-set::set() {
-  
-}
-
-set::~set() {
-  
-}
-
 int main() {
+  
+  // Load the ISD object
+  csm::Isd isd;
+  
+  // Create the plugin
+  MdisPlugin plugin;
+  
+  // Create the Sensor model
+  MdisNacSensorModel model = plugin.constructModelFromISD(isd, "ISIS_MDISNAC_USGSAstro_1_Linux64_csm30.so");
+  
+  // Test the model's accuracy
+  csm::EcefCoord groundPoint;
+  csm::ImageCoord imagePoint;
+  
+  imagePoint.samp = 300;
+  imagePoint.line = 400;
+  
+  for (int i = 0; i < 10; i++) {
+    groundPoint = model.imageToGround(imagePoint, 0);
+    
+    std::cout << "Image Point (s, l) : (" << imagePoint.samp << ", " << imagePoint.line << "); "
+              << "Ground Point (x, y, z) : (" << groundPoint.x << ", " << groundPoint.y << ", " 
+                                              << groundPoint.z << ")" << endl;
+    
+    imagePoint = model.groundToImage(groundPoint);
+  }
+  
   return 0;
 }
