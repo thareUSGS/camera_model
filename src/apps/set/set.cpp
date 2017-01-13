d34875030da21dcbc59ca7d6da2b31560f915cd4
MarjorieH <mah479@nau.edu>
2017-01-13 13:22:20 -0700

    Added set directory.

---
+++ src/apps/set/set.cpp
@@ -0,0 +1,15 @@
+#include "set.h"
+
+using namespace std;
+
+set::set() {
+  
+}
+
+set::~set() {
+  
+}
+
+int main() {
+  return 0;
+}
---
+++ src/apps/set/set.h
@@ -0,0 +1,16 @@
+#ifndef SET_H
+#define SET_H
+
+class set
+{
+  public:
+
+    set();
+    ~set();
+
+
+  private:
+
+};
+
+#endif // SET_H
