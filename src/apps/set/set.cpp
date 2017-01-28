1344ca955e75424367a7001b6eb3d19d00bb9b58
ihumphrey-usgs <ihumphrey@usgs.gov>
2017-01-28 09:28:00 -0700

    Modified SET to get ground points for each input pixel. Writes csv. #84

--- src/apps/set/set.cpp
+++ src/apps/set/set.cpp
@@ -19,6 +19,10 @@
 using namespace std;
 
 void cubeArray(vector<vector<float> > *cube, GDALRasterBand *poBand);
+void writeCSV(const string &csvFile,
+              const vector<string> &csvHeaders,
+              const vector< vector<float> > &cubeData,
+              const vector< vector<csm::EcefCoord> > &groundPoints);
 
 int main(int argc,char *argv[]) {
 
@@ -28,13 +32,16 @@ int main(int argc,char *argv[]) {
   MdisPlugin plugin;
   MdisNacSensorModel *model;
   
-   // Initialize the MdisNacSensorModel from the ISD using the plugin
+  // Initialize the MdisNacSensorModel from the ISD using the plugin
   try {
     model = dynamic_cast<MdisNacSensorModel*>
             (plugin.constructModelFromISD(*isd, "ISIS_MDISNAC_USGSAstro_1_Linux64_csm30.so"));
   }
   catch (csm::Error &e) {
     cout << e.what() << endl;
+    if (model != nullptr) {
+      delete model;
+    }
     exit (EXIT_FAILURE);
   }
 
@@ -44,24 +51,25 @@ int main(int argc,char *argv[]) {
   }
   
   // Test the model's accuracy by visually comparing the results of the function calls
-  csm::EcefCoord groundPoint;
-  csm::ImageCoord imagePoint;
-
-  for (int i = 0; i < 10; i++) {
-    groundPoint = model->imageToGround(imagePoint, 0);
-
-    std::cout << "Image Point (s, l) : (" << imagePoint.samp << ", " << imagePoint.line << "); "
-              << "Ground Point (x, y, z) : (" << groundPoint.x << ", " << groundPoint.y << ", " 
-                                              << groundPoint.z << ")" << endl;
-
-    //imagePoint = model->groundToImage(groundPoint);
-  }
+//   csm::EcefCoord groundPoint;
+//   csm::ImageCoord imagePoint;
+// 
+//   for (int i = 0; i < 10; i++) {
+//     groundPoint = model->imageToGround(imagePoint, 0);
+// 
+//     cout << "Image Point (s, l) : (" << imagePoint.samp << ", " << imagePoint.line << "); "
+//               << "Ground Point (x, y, z) : (" << groundPoint.x << ", " << groundPoint.y << ", " 
+//                                               << groundPoint.z << ")" << endl;
+// 
+//     //imagePoint = model->groundToImage(groundPoint);
+//   }
 
   //Read from a cube using the GDAL API, and outputs the DN values to a 2D vector matrix
-  string cubePath("../../../tests/data/CN0108840044M_IF_5_NAC_spiced.cub");
+  //string cubePath("../../../tests/data/CN0108840044M_IF_5_NAC_spiced.cub");
+  string cubePath { "../../../tests/data/EN1007907102M.cub" };
   GDALDataset *poDataset;
   GDALRasterBand *poBand;
-  int nBlockXSize,nBlockYSize;
+  int nBlockXSize, nBlockYSize;
 
   cout << endl;
   cout << "Testing GDAL"  << endl;
@@ -71,38 +79,35 @@ int main(int argc,char *argv[]) {
   poDataset = (GDALDataset *)GDALOpen(cubePath.c_str(),GA_ReadOnly);
 
   if (poDataset == NULL) {
-
     cout << "Could not open the:  " + cubePath  << endl;
-
   }
 
   else {
-
     poBand = poDataset->GetRasterBand(1);
-    poBand->GetBlockSize(&nBlockXSize,&nBlockYSize);
-
+    poBand->GetBlockSize(&nBlockXSize, &nBlockYSize);
+    
     cout << "Num samples = " << nBlockXSize << endl;
     cout << "Num lines = " <<   nBlockYSize << endl;
-
-  //Read a band of data
-
+    
+    //Read a band of data
     vector< vector<float> > cubeMatrix;
     cubeArray(&cubeMatrix,poBand);
-
-  //Output the values (or not if you don't want to see a long list of numbers)
-#if 0
-
-    for (int i =0; i < cubeMatrix.size(); i++ ) {
-
-      vector<float> v = cubeMatrix[i];
-      for (int j = 0; j < v.size(); j++)
-        if (j== v.size()-1)
-             cout <<"****** Line:  "<< i <<" ******" << endl;
-        else
-              cout <<" " << v[j] << " ";
-
+    
+    // Get ground X,Y,Z for each pixel in image
+    vector< vector<csm::EcefCoord> > groundPoints;
+    for (int line = 0; line < cubeMatrix.size(); line++) {
+      vector<csm::EcefCoord> groundLine;
+      for (int sample = 0; sample < cubeMatrix[line].size(); sample++) {
+        csm::ImageCoord imagePoint(line + 1, sample + 1);
+        groundLine.push_back(model->imageToGround(imagePoint, 0.0));
+      }
+      groundPoints.push_back(groundLine);
     }
-#endif
+        
+    // Write to csv file
+    string csvFilename("ground.csv");
+    vector<string> csvHeaders { "Line", "Sample", "DN", "X (km)", "Y (km)", "Z (km)" };
+    writeCSV(csvFilename, csvHeaders, cubeMatrix, groundPoints);
 
   }  //end else
  
@@ -127,5 +132,47 @@ void cubeArray(vector <vector<float> > *cube,GDALRasterBand *poBand) {
     cube->push_back(tempVector);
     tempVector.clear();
     CPLFree(pafScanline);
+  }
+}
+
+
+/**
+ * Writes a CSV file to the given destination.
+ * 
+ * This CSV will contain a DN, X, Y, and Z for each Line,Sample of the input cube data.
+ * 
+ * @param csvFilename Name of the output CSV file to write to.
+ * @param csvHeaders Vector containing the header elements to write to the CSV file.
+ * @param cubeData Matrix of cube DNs.
+ * @param groundPoints Matrix of ground points for the image pixels.
+ */
+void writeCSV(const string &csvFilename,
+              const vector<string> &csvHeaders,
+              const vector< vector<float> > &cubeData,
+              const vector< vector<csm::EcefCoord> > &groundPoints) {
+  ofstream csvFile(csvFilename);
+  if (csvFile.is_open()) {
+    
+    // Write the csv header
+    for (int str = 0; str < csvHeaders.size() - 1; str++) {
+      csvFile << csvHeaders[str] << ", ";
+    }
+    
+    // Write the last header element
+    csvFile << csvHeaders[csvHeaders.size() - 1] << "\n";
+    
+    // Write the csv records
+    for (int line = 0; line < cubeData.size(); line++) {
+      for (int sample = 0; sample < cubeData[line].size(); sample++) {
+        csm::EcefCoord ground = groundPoints[line][sample];
+        csvFile << line + 1 << ", " << sample + 1 << ", " << cubeData[line][sample] << ", "
+                << ground.x/1000 << ", " << ground.y/1000 << ", " << ground.z/1000 << "\n";
+      }
+    }
+  }
+  
+  else {
+    cout << "\nUnable to open file \"" << csvFilename << " for writing." << endl;
+    exit(EXIT_FAILURE);
   }
 }
